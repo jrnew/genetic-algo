3e6356b5dc0aa105b73ec708c91b249d69cf5a59
Jin Rou New <newjinrou@gmail.com>
2014-11-30 23:09:09 -0800

    Edited recombine and mutate functions.R

--- R/mutate.R
+++ R/mutate.R
@@ -4,13 +4,14 @@
 #' 
 #' @param pop Matrix of population of chromosomes
 #' @param mutation_rate Numeric, between 0 and 1; Default is 0.01.
+#' @return A matrix of population of chromosomes that have undergone mutation.
 mutate <- function(
   pop,
   mutation_rate = 0.01
 ) {
-  ngenes <- prod(dim(pop))
+  num_genes_total <- prod(dim(pop))
   # Probabilty of getting a 1 is mutation_rate if gene is currently 0 and vice versa
-  pop_mutate <- matrix(rbinom(ngenes, rep(1, ngenes), 
+  pop_mutate <- matrix(rbinom(num_genes_total, rep(1, num_genes_total), 
                               ifelse(c(pop) == 0, mutation_rate, 1-mutation_rate)))
   return(pop_mutate)
 }
--- R/recombine.R
+++ R/recombine.R
@@ -1,10 +1,20 @@
 #' Recombine.
 #' 
-#' Recombine.
+#' Carry out crossover of parent chromosomes.
 #' 
 #' @param pop Matrix of population of chromosomes.
+#' @param method String; One of "single", "double", "all"
 recombine <- function(
-  pop
+  pop,
+  method = "single"
 ) {
 # pick a pair of strings, with prob pc recombine 
+# crossover at single random point
+# crossover at all genes
+  if ("single") {
+    breakpoint <- ceiling(runif(1, 0, 10))
+    child <- c(parent1[1:breakpoint], parent2[(breakpoint + 1):num_genes])
+  } else if ("all") {
+    child <- ifelse(rbinom(num_genes, 1, 0.5) == 1, parent1, parent2)
+  }
 }
