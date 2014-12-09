8e11f2c636166a9e940f801d10f4b01677b18587
Yang Hu <hu002@berkeley.edu>
2014-12-08 23:48:50 -0800

    changed output format for summary function

--- R/summary_ga.R
+++ R/summary_ga.R
@@ -10,11 +10,17 @@ summary.ga <- function(ga_result)
   rank <- order(ga_result$log$evaluation_best)
   top5 <- ga_result$log$models[rank,][1:5,]
   score <- sort(ga_result$log$evaluation_best)[1:5]
-  df <- data.frame(top5,score)
-  names(df) <- c(ga_result$model_data$xvars, "score")
-  return(df)
-}
 
+  for (i in 1:5){
+  x <- ga_result$model_data$xvars[which(top5[i,]==1)]
+  y <- ga_result$model_data$yvar
+  right <- paste(x,collapse = "+")
+  cat("Model", i,":", "\n",
+      paste(c(y,right), collapse = "="), "\n",
+      "AIC", "=", score[i], "\n")
+  }
+  
+}
 #Test
 #ga_result <- ga(data,yvar = yvar, xvars = xvars, num_max_iterations = 10L, method_select = "DS")
 #summary.ga(ga_result)
